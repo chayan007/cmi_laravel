bd6e3976ac8b44ea820e9bab921d74845ff1d507
Chayan Datta <sonicxxx7@gmail.com>
2019-01-28 22:05:13 +0530

    Alphabetical order

--- app/Http/Controllers/PublicController.php
+++ app/Http/Controllers/PublicController.php
@@ -22,26 +22,26 @@ class PublicController extends Controller
 
     public function products()
     {
-        $products = Product::orderBy('created_at', 'desc')->paginate(20);
+        $products = Product::orderBy('name')->paginate(20);
         return view('public.pages.shop', ['pro_ducts' => $products]);
     }
 
     public function productByCategory($id)
     {
-        $products = Product::where('category_id', $id)->orderBy('created_at', 'desc')->paginate(20);
+        $products = Product::where('category_id', $id)->orderBy('name')->paginate(20);
         return view('public.pages.shop', ['pro_ducts' => $products]);
     }
 
     public function productByCompany($id)
     {
-        $products = Product::where('company_id', $id)->orderBy('created_at', 'desc')->paginate(20);
+        $products = Product::where('company_id', $id)->orderBy('name')->paginate(20);
         return view('public.pages.shop', ['pro_ducts' => $products]);
     }
 
     public function singleProduct($id)
     {
         $product = Product::where('id', $id)->firstOrFail();
-        $recommendations = Product::where('category_id', $product->category_id)->orderBy('created_at', 'desc')->paginate(10);
+        $recommendations = Product::where('category_id', $product->category_id)->orderBy('created_at','desc')->paginate(10);
         return view('public.pages.product',[
             'product' => $product,
             'recommendations' => $recommendations,
